e98fa11626b53e22cf7a6b050b1b32fe74fc327b
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-05-24 09:54:44 +0530

    New Color for tkinter

--- Tk_prog.py
+++ Tk_prog.py
@@ -2,6 +2,6 @@ from tkinter import *
 root = Tk()
 root.title("My Main Window")
 root.geometry("1000x400")
-root.configure(bg='red')
+root.configure(bg='white')
 root.wm_iconbitmap("Python Class\ACBF_PC.ico")
 root.mainloop()
