b27d10e33d42ae198839cafeacbb44b0ea335df7
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-05-24 09:44:57 +0530

    Added files for tkinter

---
+++ Tk_prog.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+root.title("My Main Window")
+root.geometry("1000x400")
+root.configure(bg='red')
+root.wm_iconbitmap("Python Class\ACBF_PC.ico")
+root.mainloop()
